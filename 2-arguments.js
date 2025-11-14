9e9e1aac9b61320cb2f8e984597a944ed55fd5dd
Oke Muhammed Abiodun <okemuhammed123@gmail.com>
2025-11-14 01:54:05 +0100

    Add hello.js with initial JavaScript code

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,8 +1,8 @@
 const args = (process.argv.slice(2));
 
-if (args.length === 0) {
+if (args === 0) {
   console.log('No argument');
-} else if (args.length === 1) {
+} else if (args === 1) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
--- 3-value_argument.js
+++ 3-value_argument.js
@@ -3,7 +3,7 @@ const args2 = process.argv[3];
 if (!args1){
   console.log('No argument');
 } else if (!args2){
-  console.log(`One argument found${args1} ${args2}`);
+  console.log(`One argument:${args1}`);
 } else {
-  console.log('Arguments found');
+  console.log('2 arguments: ${args1}, ${args2}');
 }
