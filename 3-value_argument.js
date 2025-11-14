fa85d631cc2a2e2f6585914297baae73b1b230cc
Oke Muhammed Abiodun <okemuhammed123@gmail.com>
2025-11-15 00:46:31 +0100

    O

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -1,9 +1,10 @@
 const args1 = process.argv[2];
 const args2 = process.argv[3];
-if (!args1){
+
+if (!args1) {
   console.log('No argument');
-} else if (!args2){
+} else if (!args2) {
   console.log(args1);
 } else {
-  console.log(`2 arguments: "${args1} ${args2}"`)
+  console.log(`2 arguments: "${args1} ${args2}"`);
 }
