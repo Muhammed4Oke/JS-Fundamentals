754207e3d742311c3a0f1115fbf1b9e897c1a48b
Oke Muhammed Abiodun <okemuhammed123@gmail.com>
2025-11-14 01:42:00 +0100

    Trying again

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -1,9 +1,9 @@
-const args = (process.argv.slice(2));
-
-if (args.length === 0) {
+const args1 = process.agrv[2];
+const args2 = process.agrv[3];
+if (!args1){
   console.log('No argument');
-} else if (args.length === 1) {
-  console.log('Argument found');
+} else if (!args2){
+  console.log(`One argument found${args1}`);
 } else {
   console.log('Arguments found');
 }
