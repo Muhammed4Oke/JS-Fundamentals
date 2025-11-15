862442440f760d70ecb08741b7ea6922fea4fa10
Oke Muhammed Abiodun <okemuhammed123@gmail.com>
2025-11-15 01:24:25 +0100

    M

---
+++ 5-to_integer.js
@@ -0,0 +1,7 @@
+const arg = process.argv[2];
+const agr2 = parseInt(process.argv[2]);
+if(arg2 === 1 & 2 & 3 & 4 & 5 & 6 & 7 & 8 & 9 & 10){
+  console.log(`My number: ${arg2}`);
+}else{
+    console.log('Not a number');
+  }
