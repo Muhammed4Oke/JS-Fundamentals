bb9c74ac1448898e5872631caaee955a9efe095d
Oke Muhammed Abiodun <okemuhammed123@gmail.com>
2025-11-16 21:36:07 +0100

    K

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,7 +1,7 @@
 const arg = process.argv[2];
 const num = parseInt(arg, 10);
 
-if (!isNaN(num) && num > 0) {
+if (!isNaN(num)) {
   for (let i = 0; i < num; i++) {
     console.log("C is fun");
   }
