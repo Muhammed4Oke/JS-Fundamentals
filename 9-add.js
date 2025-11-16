e8f2fc415870671c78841f9be292d9a870d010f9
Oke Muhammed Abiodun <okemuhammed123@gmail.com>
2025-11-16 22:10:20 +0100

    K

---
+++ 9-add.js
@@ -0,0 +1,11 @@
+const arg = process.argv[2];
+const arg1 = process.argv[3];
+const num1 = parseInt(arg, 10);
+const num2 = parseInt(arg1, 10);
+function add(num1, num2) {
+    return num1 + num2;
+}
+console.log(add(num1, num2));
+if (isNaN(num1) && isNaN(num2)) {
+    console.log("NaN");
+}
