b37caef6a11ddb56d58a17a0a0f2686115f0be60
Oke Muhammed Abiodun <okemuhammed123@gmail.com>
2025-11-16 21:55:30 +0100

    MM

---
+++ 8-square.js
@@ -0,0 +1,9 @@
+const arg = process.argv[2];
+const num = parseInt(arg, 10);
+if (isNaN(num)) {
+    console.log("Missing size")
+}
+else {
+    for (let i = 0; i < num; i++)
+    console.log("X");
+}
