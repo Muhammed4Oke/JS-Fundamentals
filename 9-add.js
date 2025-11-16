83f290acb22f8fa9233c2168cb99cb892b497d2d
Oke Muhammed Abiodun <okemuhammed123@gmail.com>
2025-11-16 22:57:49 +0100

    adf

--- 9-add.js
+++ 9-add.js
@@ -5,10 +5,11 @@ const num2 = parseInt(arg1, 10);
 if (arg === undefined || arg1 === undefined) {
     console.log("NaN");
 }
+else {
 function add(num1, num2) {
     return num1 + num2;
 }
 console.log(add(num1, num2));
 if (isNaN(num1) && isNaN(num2)) {
     console.log("NaN");
-}
+}}
